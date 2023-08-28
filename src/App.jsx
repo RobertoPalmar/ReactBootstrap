d3be0d7af8e8c6538853c0a429612432689bb902
RobertoPalmar <roberto.palmar.c@gmail.com>
2023-08-27 20:27:18 -0400

    Detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Button, Col, Container, Nav, NavDropdown, Navbar, Row, Stack, ThemeProvider } from "react-bootstrap";
+import { Button, Col, Container, Dropdown, Image, Nav, NavDropdown, Navbar, Row, Stack, ThemeProvider } from "react-bootstrap";
 
 function App() {
   return (
@@ -20,8 +20,10 @@ function App() {
           <Navbar.Collapse>
             <Nav className="ms-auto" navbarScroll>
               <Nav.Link>Inicio</Nav.Link>
-              <NavDropdown title="Categoria">
-                <NavDropdown.Item className="bg-primary">SASS</NavDropdown.Item>
+              <NavDropdown title="Categoria" className="me-md-4">
+                <Dropdown.Item as={Button} className="bg-info text-black w-100 fw-normal">SASS</Dropdown.Item>
+                <Dropdown.Item as={Button} className="bg-info text-black w-100 fw-normal">Bootstrap</Dropdown.Item>
+                <Dropdown.Item as={Button} className="bg-info text-black w-100 fw-normal">React</Dropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
@@ -30,12 +32,28 @@ function App() {
 
       <Container
         fluid
-        className="App py-3 bg-dark vh-100"
+        className="App bg-dark vh-100 pt-4 "
       >
-        <Stack gap={2} className="my-2 col-10 col-md-8 col-lg-5 mx-auto">
-          <Button variant="primary">Aceptar</Button>
-          <Button variant="secondary">Cancelar</Button>
-        </Stack>
+
+        <Row className="mx-1 mx-md-5">
+          <Col xs={12} md={6}>
+            <Image
+              src="../img/main.jpg"
+              fluid rounded
+              className="my-3 img-md"
+            />
+            {/* <div className="bg-secondary p-2 my-2 text-white rounded">Contenido A</div> */}
+          </Col>
+          <Col xs={12} md={6}>
+            {/* <div className="bg-primary p-2 my-2 text-white rounded">Contenido B</div> */}
+            <Stack gap={2} className="my-3 px-2 col-10 w-100">
+              <h2 className="text-white text-center">Titulo de Prueba</h2>
+              <p className="text-white text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci atque aliquid culpa, veniam officiis vitae porro accusamus laudantium ad rem! Eligendi eum laborum quis inventore consequuntur maxime ipsa ea. Recusandae.</p>
+              {/* <Button variant="primary">Aceptar</Button>
+              <Button variant="secondary">Cancelar</Button> */}
+            </Stack>
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
